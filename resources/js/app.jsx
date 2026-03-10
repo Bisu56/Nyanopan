dae587e09837c440f7c003e80d3b28bb7f01e505
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-10 23:40:17 +0545

    frontend component

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Frontend from './components/Frontend';
+
+import './bootstrap';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Frontend />
+    </React.StrictMode>
+);
