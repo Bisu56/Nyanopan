4953a3bfb8b8a7e183e7c3b6278db4192ce65df6
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-11 23:35:00 +0545

    update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Frontend from './components/Frontend';
+import Categories from './components/Categories';
+import Products from './components/Products';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import Orders from './components/Orders';
+import Contact from './components/Contact';
+import About from './components/About';
 
 import './bootstrap';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Frontend />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Frontend />} />
+                <Route path="/category" element={<Categories />} />
+                <Route path="/view-category/:slug" element={<Products />} />
+                <Route path="/view-product/:slug" element={<ProductDetail />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/my-order" element={<Orders />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
