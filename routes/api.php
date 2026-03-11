63d9f02e3017b88e59552d45ca3857636cf6257c
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-11 23:39:15 +0545

    api functions

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\frontendController;
+use App\Http\Controllers\frontend\CartController;
+use App\Http\Controllers\frontend\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,13 +10,22 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Public API Routes
+Route::get('/get-category', [frontendController::class, 'getCategory']);
+Route::get('/view-category/{slug}', [frontendController::class, 'viewCategory']);
+Route::get('/view-product/{slug}', [frontendController::class, 'eachProdView']);
+Route::get('/cart', [CartController::class, 'viewCartApi']);
+Route::get('/api/my-order', [UserController::class, 'indexApi']);
+
+// Authenticated API Routes
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/add-to-cart', [CartController::class, 'addProduct']);
+    Route::post('/delete-cart-item', [CartController::class, 'deleteProduct']);
+    Route::post('/update-cart', [CartController::class, 'updateCart']);
 });
