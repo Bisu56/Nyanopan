fb329aaf14bf2e58b7604ea490286aa89df93b9f
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-17 21:30:11 +0545

    shop controller

--- app/Http/Controllers/Admin/frontendController.php
+++ app/Http/Controllers/Admin/frontendController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Product;
 
 class frontendController extends Controller
 {
     public function index()
     {
         return view('Admin.dashboard');
+    }
+
+    public function getCategory()
+    {
+        $category = Category::latest()->get();
+
+        return response()->json(['category' => $category]);
+    }
+
+    public function getProducts()
+    {
+        $products = Product::latest()->get();
+
+        return response()->json(['products' => $products]);
     }
 }
