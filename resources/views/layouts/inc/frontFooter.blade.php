0b7578d33139842619fe7685245e9cb8705821b2
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-20 23:43:30 +0545

    footer for dynamic

--- resources/views/layouts/inc/frontFooter.blade.php
+++ resources/views/layouts/inc/frontFooter.blade.php
@@ -3,35 +3,29 @@
     <div class="container p-4 pb-0">
       <!-- Section: Social media -->
       <section class="mb-4">
-        <!-- Facebook -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        @if(isset($site_settings['facebook_url']) && $site_settings['facebook_url'])
+        <a class="btn btn-outline-light btn-floating m-1" href="{{$site_settings['facebook_url']}}" role="button"
           ><i class="fab fa-facebook-f"></i
         ></a>
+        @endif
   
-        <!-- Twitter -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        @if(isset($site_settings['twitter_url']) && $site_settings['twitter_url'])
+        <a class="btn btn-outline-light btn-floating m-1" href="{{$site_settings['twitter_url']}}" role="button"
           ><i class="fab fa-twitter"></i
         ></a>
+        @endif
   
-        <!-- Google -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-google"></i
-        ></a>
-  
-        <!-- Instagram -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        @if(isset($site_settings['instagram_url']) && $site_settings['instagram_url'])
+        <a class="btn btn-outline-light btn-floating m-1" href="{{$site_settings['instagram_url']}}" role="button"
           ><i class="fab fa-instagram"></i
         ></a>
-  
-        <!-- Linkedin -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-linkedin-in"></i
+        @endif
+
+        @if(isset($site_settings['youtube_url']) && $site_settings['youtube_url'])
+        <a class="btn btn-outline-light btn-floating m-1" href="{{$site_settings['youtube_url']}}" role="button"
+          ><i class="fab fa-youtube"></i
         ></a>
-  
-        <!-- Github -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-github"></i
-        ></a>
+        @endif
       </section>
       <!-- Section: Social media -->
     </div>
@@ -39,8 +33,8 @@
   
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2025 Copyright:
-      <a class="text-white" href="/">Developed By Bishal</a>
+      © {{date('Y')}} Copyright:
+      <a class="text-white" href="/">{{ $site_settings['site_name'] ?? 'Nyanopan' }}</a>
     </div>
     <!-- Copyright -->
   </footer>
